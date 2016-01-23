6355720d367caf3a6ae18250fb81bee8b42797af
up637415 <up637415@myport.ac.uk>
2016-01-23 19:58:19 +0000

    More transition testing.

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,39 +1,6 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
   console.log( "ready!" );
-  // demo for hooking the Android backbutton to the slide 'right'
-  document.addEventListener('backbutton', function() {
-    slide('right', 'purple');
-  }, false);
-
-  // Slide for Android
-  function slide(direction, color, slowdownfactor, hrf) {
-    if (!hrf) {
-      setTimeout(function () {
-        // update the page inside this timeout
-        document.querySelector("#title").innerHTML = direction;
-        document.querySelector("html").style.background = color;
-      }, 20);
-    }
-    // not passing in options makes the plugin fall back to the defaults defined in the JS API
-    var theOptions = {
-      'direction': direction,
-      'duration': 2000,
-      'slowdownfactor' : slowdownfactor,
-      'href': hrf,
-      'fixedPixelsTop' : 40, // optional, the number of pixels of your fixed header, default 0 (iOS and Android)
-      'fixedPixelsBottom': 0  // optional, the number of pixels of your fixed footer (f.i. a tab bar), default 0 (iOS and Android)
-    };
-    window.plugins.nativepagetransitions.slide(
-        theOptions,
-        function () {
-          console.log('------------------- slide transition finished');
-        },
-        function (msg) {
-          alert('error: ' + msg);
-        });
-  }
-
   $('#decrease').click(function() {
     $('p, h2, .mobile-button-big').css('font-size', function(i, v) {
         return (+v.match(/\d+/).join() - 2) + 'px';
@@ -46,3 +13,19 @@ $( document ).ready(function() {
     });
   });
 });
+
+var options = {
+  "direction"        : "up", // 'left|right|up|down', default 'left' (which is like 'next')
+  "duration"         :  500, // in milliseconds (ms), default 400
+  "slowdownfactor"   :    3, // overlap views (higher number is more) or no overlap (1), default 4
+  "iosdelay"         :  100, // ms to wait for the iOS webview to update before animation kicks in, default 60
+  "androiddelay"     :  150, // same as above but for Android, default 70
+  "winphonedelay"    :  250, // same as above but for Windows Phone, default 200,
+  "fixedPixelsTop"   :    0, // the number of pixels of your fixed header, default 0 (iOS and Android)
+  "fixedPixelsBottom":   60  // the number of pixels of your fixed footer (f.i. a tab bar), default 0 (iOS and Android)
+};
+window.plugins.nativepagetransitions.slide(
+  options,
+  function (msg) {console.log("success: " + msg)}, // called when the animation has finished
+  function (msg) {alert("error: " + msg)} // called in case you pass in weird values
+);
