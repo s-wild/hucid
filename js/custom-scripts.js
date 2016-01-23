709e40fc7f62589b2abd9a706aa9950e0ef1dad3
up637415 <up637415@myport.ac.uk>
2016-01-23 20:00:54 +0000

    More transition testing.

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -14,18 +14,7 @@ $( document ).ready(function() {
   });
 });
 
-var options = {
-  "direction"        : "up", // 'left|right|up|down', default 'left' (which is like 'next')
-  "duration"         :  500, // in milliseconds (ms), default 400
-  "slowdownfactor"   :    3, // overlap views (higher number is more) or no overlap (1), default 4
-  "iosdelay"         :  100, // ms to wait for the iOS webview to update before animation kicks in, default 60
-  "androiddelay"     :  150, // same as above but for Android, default 70
-  "winphonedelay"    :  250, // same as above but for Windows Phone, default 200,
-  "fixedPixelsTop"   :    0, // the number of pixels of your fixed header, default 0 (iOS and Android)
-  "fixedPixelsBottom":   60  // the number of pixels of your fixed footer (f.i. a tab bar), default 0 (iOS and Android)
-};
-window.plugins.nativepagetransitions.slide(
-  options,
-  function (msg) {console.log("success: " + msg)}, // called when the animation has finished
-  function (msg) {alert("error: " + msg)} // called in case you pass in weird values
-);
+window.plugins.nativepagetransitions.slide({
+    // the defaults for direction, duration, etc are all fine
+    "href" : "pages/register.html"
+});
